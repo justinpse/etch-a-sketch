8b7ee4f306f88b8af70be171e2a0d3dd1915cc8d
Caldwell <99009777+justinpse@users.noreply.github.com>
2024-03-22 16:25:02 +0800

    Add default grid

--- script.js
+++ script.js
@@ -1,33 +1,39 @@
-const rowNum = 16;
-const colNum = 16;
+function defaultGrid() {
+    const rowNum = 16;
+    const colNum = 16;
 
-let container = document.getElementById('container');
+    let container = document.getElementById('container');
 
-for (let i = 0; i < rowNum; i++) {
-    let row = document.createElement('div');
-    row.className = 'row';
-    row.style.height = (container.clientHeight / rowNum) + 'px';
-    row.style.width = container.clientWidth + 'px';
+    for (let i = 0; i < rowNum; i++) {
+        let row = document.createElement('div');
+        row.className = 'row';
+        row.style.height = (container.clientHeight / rowNum) + 'px';
+        row.style.width = container.clientWidth + 'px';
 
-    for (let i = 0; i < colNum; i++) {
-        let col = document.createElement('div');
-        col.className = 'square';
-        col.style.height = row.style.height;
-        col.style.width = (container.clientWidth / colNum) + 'px';
+        for (let i = 0; i < colNum; i++) {
+            let col = document.createElement('div');
+            col.className = 'square';
+            col.style.height = row.style.height;
+            col.style.width = (container.clientWidth / colNum) + 'px';
 
-        row.appendChild(col);
+            row.appendChild(col);
+        };
+
+        container.appendChild(row);
     };
 
-    container.appendChild(row);
+    let cells = document.getElementsByClassName('square');
+
+    for (let i = 0; i < cells.length; i++) {
+        cells[i].addEventListener('mouseenter', () => {
+            cells[i].style.backgroundColor = 'black';
+        });
+    };
 };
 
-let cells = document.getElementsByClassName('square');
-
-for (let i = 0; i < cells.length; i++) {
-    cells[i].addEventListener('mouseenter', () => {
-        cells[i].style.backgroundColor = 'black';
-    });
-};
+document.addEventListener("DOMContentLoaded", function() {
+    defaultGrid();
+})
 
 let size;
 
